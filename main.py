c7f21d37d07df3f79e453afa4ca2625f28fbe33a
Sa2Knight <shingo.sasaki.0529@gmail.com>
2017-08-25 16:07:57 +0000

    リポジトリを作成し、現状をコミット

---
+++ main.py
@@ -0,0 +1,29 @@
+import requests
+import json
+
+EVENTS_URL     = "https://api.github.com/users/sa2knight/events"
+REPOSITORY_URL = "https://api.github.com/repos"
+
+def get_repository_description(repo_name):
+  url = f"{REPOSITORY_URL}/{repo_name}"
+  response = requests.get(url)
+  repository = json.loads(response.text)
+  return repository['description']
+
+def get_recent_push_event():
+  response = requests.get(EVENTS_URL)
+  events   = json.loads(response.text)
+  recent_event = list(filter(lambda e: e['type'] == 'PushEvent', events))[0]
+  repo_name    = recent_event['repo']['name']
+  commits      = list(map(lambda c: parse_commit_log(repo_name, c), recent_event['payload']['commits']))
+  return {
+    'id':         recent_event['id'],
+    'repository': get_repository_description(recent_event['repo']['name']),
+    'commits':    commits,
+  }
+
+def parse_commit_log(repo_name, commit):
+  return {
+    'url': f"https://github.com/{repo_name}/commit/{commit['sha']}",
+    'message': commit['message']
+  }
