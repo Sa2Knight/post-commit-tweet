16d7928ad27502a8de9320a4467b8406914c3bf2
Sa2Knight <shingo.sasaki.0529@gmail.com>
2017-08-26 03:29:03 +0000

    ツイートメソッドを実装

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
+import twitter
 import requests
 import json
+import os
 
 EVENTS_URL     = "https://api.github.com/users/sa2knight/events"
 REPOSITORY_URL = "https://api.github.com/repos"
+
+def tweet(text):
+  auth = twitter.OAuth(consumer_key=os.environ['TWITTER_CONSUMER_KEY'],
+                       consumer_secret=os.environ['TWITTER_CONSUMER_SECRET'],
+                       token=os.environ['TWITTER_ACCESS_TOKEN'],
+                       token_secret=os.environ['TWITTER_ACCESS_SECRET'])
+  t = twitter.Twitter(auth=auth)
+  t.statuses.update(status=text)
+
+def parse_commit_log(repo_name, commit):
+  return {
+    'url': f"https://github.com/{repo_name}/commit/{commit['sha']}",
+    'message': commit['message']
+  }
 
 def get_repository_description(repo_name):
   url = f"{REPOSITORY_URL}/{repo_name}"
@@ -22,20 +38,12 @@ def get_recent_push_event():
     'commits':    commits,
   }
 
-def parse_commit_log(repo_name, commit):
-  return {
-    'url': f"https://github.com/{repo_name}/commit/{commit['sha']}",
-    'message': commit['message']
-  }
-
 event = get_recent_push_event()
-tweet = f"""
+tweet_text = f"""
 @null
 Githubにコミットをプッシュしました。
 [{event['repository']}] 「{event['commits'][0]['message']}」
 """.strip()
 if 1 < len(event['commits']):
-  tweet += f"ほか{len(event['commits']) - 1}件"
-tweet += f"\n\n{event['commits'][0]['url']}"
-
-print(tweet)
+  tweet_text += f"ほか{len(event['commits']) - 1}件"
+tweet_text += f"\n\n{event['commits'][0]['url']}"
